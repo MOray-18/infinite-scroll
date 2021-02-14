a0e66455a5e8ad3ab9ced9e716702234cd9fbe0c
Moray <mrnabar@gmail.com>
2021-02-14 14:20:07 +0530

    add function to toggle spinner display

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // elements to manipulate
 const imageContainer = document.querySelector('.image-container');
+const spinner = document.querySelector('.loader');
 
 // Unsplash API
 const apiKey = 'fXDRTXcJ_WAvxKK-UQZOWQGHE6hej9CXtSdxlbYvcwE';
 const count = 10;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// control spinner display
+const controlSpinner = function () {
+  const { hidden } = spinner;
+  spinner.hidden = !hidden;
+  imageContainer.hidden = hidden;
+};
+
 // get photos from API
 const getPhotos = async function () {
   try {
+    controlSpinner();
+
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
+
+    controlSpinner();
 
     renderImages(data);
   } catch (error) {
