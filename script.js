df95a07ab6b9ec10f605946097430f92213b24fc
Moray <mrnabar@gmail.com>
2021-02-14 08:44:35 +0530

    add function to render images

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// elements to manipulate
+const imageContainer = document.querySelector('.image-container');
+
 // Unsplash API
 const apiKey = 'fXDRTXcJ_WAvxKK-UQZOWQGHE6hej9CXtSdxlbYvcwE';
 const count = 10;
@@ -5,9 +8,24 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 
 // get photos from API
 const getPhotos = async function () {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(apiUrl);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+
+    renderImages(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderImages = function (images) {
+  images.forEach(img => {
+    const { alt_description: alt, links } = img;
+    const imageEl = `<img src="${links.html}" alt="${alt}" />`;
+    imageContainer.insertAdjacentHTML('beforeend', imageEl);
+  });
 };
 
 getPhotos();
